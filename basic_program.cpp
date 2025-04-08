b6b577577388225fd1d81bd37daa1e5c49dba259
nahimulbs23 <nahimul.islam@brainstation-23.com>
2025-04-08 19:22:25 +0600

    Add a factorial function to calculate factorial of a number

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int num;
+    cout << "Enter a number to calculate its factorial: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
     return 0;
 }
