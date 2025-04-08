9b15fae20da5a7e565d4769659c6038f9d56f2db
nahimulbs23 <nahimul.islam@brainstation-23.com>
2025-04-08 19:22:25 +0600

    Cherry-pick critical fix from feature-branch

--- basic_program.cpp
+++ basic_program.cpp
@@ -8,24 +8,12 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
 int main() {
     int num;
     cout << "Enter a number to calculate its factorial: ";
     cin >> num;
 
     cout << "Factorial of " << num << " is " << factorial(num) << endl;
-
-    cout << "Enter a number to calculate its Fibonacci: ";
-    cin >> num;
-
-    cout << "Fibonacci number at position " << num << " is " << fibonacci(num) << endl;
 
     return 0;
 }
