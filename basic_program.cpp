64dcb89d00992fd67287dcd54b0e882dea071b40
nahimulbs23 <nahimul.islam@brainstation-23.com>
2025-04-09 12:18:15 +0600

    Add a new problem, such as finding the nth prime number.

--- basic_program.cpp
+++ basic_program.cpp
@@ -8,6 +8,25 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int nthPrime(int n) {
+    int count = 0, num = 2;
+    while (true) {
+        if (isPrime(num)) {
+            count++;
+            if (count == n) return num;
+        }
+        num++;
+    }
+}
+
 int main() {
     int num;
     cout << "Enter a number to calculate its factorial: ";
